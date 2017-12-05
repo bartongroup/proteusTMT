baa8de382e7a7af1af1d58e39918430bbc757700
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2017-12-05 12:58:43 +0000

    Initial import

---
+++ R/proteusTMT.R
@@ -0,0 +1,57 @@
+#' @title Proteus example TMT data
+#' @name proteusTMT
+#' @docType data
+#' @description Example data set for \code{proteus} package. It comes from a TMT MS/MS experiment in two conditions
+#' and three replicates each.
+#'
+#' @format Four objects from different stages of data processing:
+#' \describe{
+#'   \item{\code{evi}}{Pre-processed evidence data (data frame)}
+#'   \item{\code{meta}}{Metadata (data frame)}
+#'   \item{\code{pepdat}}{Peptide data (\code{proteusData} object)}
+#'   \item{\code{prodat}}{Protein data (\code{proteusData} object)}
+#' }
+#'
+#' @examples
+#' data(proteusTMT)
+#' meta
+#' head(pepdat$tab)
+#' head(prodat$tab)
+#"proteusTMT"
+NULL
+
+#' Proteomics metadata
+#'
+#' Metadata describing a TMT proteomics experiment in 5 conditions with 3 replicates each.
+#'
+#' @docType data
+#' @name meta
+#' @format A \code{data.frame} with 30 rows and 5 columns (experiment, measure, sample, replicate).
+NULL
+
+#' Evidence data
+#'
+#' Evidence data from a TMT proteomics experiment in 5 conditions, 3 replicates each.
+#'
+#' @docType data
+#' @name evi
+#' @format A data frame with 601,792 rows and 19 columns
+NULL
+
+#' Peptide data
+#'
+#' Peptide data aggregated from evidence data \code{evi}. Contains all replicates.
+#'
+#' @docType data
+#' @name pepdat
+#' @format A \code{proteusData} object
+NULL
+
+#' Proteins data
+#'
+#' Protein data aggregated from evidence data \code{evi}.
+#'
+#' @docType data
+#' @name prodat
+#' @format A \code{proteusData} object
+NULL
